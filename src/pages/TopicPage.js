80f2533f019fcb3134a2e801bd5618cbd36b56d6
Hamza EL KHATIB <contact@hamzaelkhatib.com>
2025-05-11 18:55:30 +0100

    feat: remove unused Boot JSON import from TopicPage

--- src/pages/TopicPage.js
+++ src/pages/TopicPage.js
@@ -9,7 +9,6 @@ import exceptionsJson from '../data/java/exceptions.json';
 import javaFundamentalsJson from '../data/java/java-fundamentals.json';
 
 // Spring imports
-import bootJson from '../data/spring/boot.json';
 import dataJson from '../data/spring/data.json';
 import restJson from '../data/spring/rest.json';
 //import securityJson from '../data/spring/security.json';
@@ -39,7 +38,6 @@ const jsonData = {
         fundamentals: javaFundamentalsJson,
     },
     spring: {
-        boot: bootJson,
         data: dataJson,
         rest: restJson,
         //security: securityJson
